ae10653c659fc3eeb8e5adcd7309bb58517158ed
Damian Lewandowski <125447750+d7lewandowski@users.noreply.github.com>
2025-07-16 14:46:52 +0000

    oop

---
+++ OOP/Zombie.py
@@ -0,0 +1,21 @@
+from Enemy import *
+import random
+
+class Zombie(Enemy):
+
+    def __init__(self, health_points, attack_damage):
+        super().__init__(type_of_enemy="Zombie", health_points=health_points, attack_damage=attack_damage)
+
+
+
+    def talk(self):
+        print('*Grumbling*..')
+
+    def spread_disease(self):
+        print("The zombie is trying a infection")
+
+    def special_attack(self):
+        did_speacial_attack_work = random.random() < 0.50
+        if did_speacial_attack_work:
+            self.health_points += 2
+            print("Zombie regenereated 2HP")
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FastAPI
+
+
+Source code:
+git clone https://github.com/codingwithroby/fastapi-the-complete-course.git
